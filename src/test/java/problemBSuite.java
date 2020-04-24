e88cb92abc9f09c8dbc1b1e230e7d2617cd2b5a0
dudiadikaryaa <ilhamdudi16@gmail.com>
2020-04-24 23:01:08 +0700

    DONE RUN SUCCES :')

--- src/test/java/problemBSuite.java
+++ src/test/java/problemBSuite.java
@@ -3,6 +3,6 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features/problemA.feature")
+@CucumberOptions(features="src/test/resources/features/problemB.feature")
 public class problemBSuite {
 }
